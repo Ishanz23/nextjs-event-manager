b2a91db63beaea48b9c50a4542ca9dd72cb61fdd
Sayantan Dey <sayantandey23@gmail.com>
2021-05-10 14:49:27 +0530

    fix: import order sorted

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,9 @@
+import { useRouter } from 'next/router'
 import { Fragment } from 'react'
 import { getAllEvents } from '../../dummy-data'
+
 import EventList from '../../components/events/EventList'
 import EventsSearch from '../../components/events/EventsSearch'
-import { useRouter } from 'next/router'
 
 export default function AllEventsPage() {
   const router = useRouter()
